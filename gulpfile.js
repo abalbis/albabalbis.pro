4d8e2c416860f66ebb0d69b2a3e9f02d75fdb672
“abalbis” <abmoriana@gmail.com>
2024-10-05 00:07:44 +0200

    make a dist dev and upload styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require("gulp");
 const fileInclude = require("gulp-file-include");
-gulp.task("html", function () {
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+
+gulp.task("compile", function () {
   return gulp
     .src(["src/index.html"])
     .pipe(
@@ -9,6 +12,28 @@ gulp.task("html", function () {
         basepath: "@file",
       })
     )
-    .pipe(gulp.dest("docs/dist"));
+    .pipe(gulp.dest("dist"));
 });
-gulp.task("default", gulp.series("html"));
+
+gulp.task("css", function () {
+  return gulp
+    .src("src/styles/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src("dist/**/*").pipe(gulp.dest("docs"));
+});
+
+gulp.task("dev", gulp.series("compile", "css"));
+gulp.task("release", gulp.series("compile", "css", "copy"));
+
+gulp.task("serve", function () {
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+    },
+  });
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
+});
