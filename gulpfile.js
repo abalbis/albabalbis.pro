c8a66000a61a8f87869baef80d088f8e7b06e7c3
“abalbis” <abmoriana@gmail.com>
2024-10-04 20:48:17 +0200

    dist fix error to compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const fileInclude = require("gulp-file-include");
 
 gulp.task("html", function () {
   return gulp
-    .src(["src/index.html"])
+    .src("docs/*.html")
     .pipe(
       fileInclude({
         prefix: "@@",
         basepath: "@file",
       })
     )
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("docs/dist"));
 });
 
 gulp.task("default", gulp.series("html"));
